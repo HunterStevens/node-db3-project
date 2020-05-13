04cac1d394a0981bbd532e6f432b36c0c7c023b9
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-13 16:53:12 -0600

    Find model helpers working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,15 +1,24 @@
 const db = require('../data/dbConfig');
 
-function find(){
+module.exports ={
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
 
+function find(){
+    return db('schemes');
 }
 
 function findById(id){
-    
+    return db('schemes').where({id}).first();
 }
 
 function findSteps(id){
-    
+    return db('steps').where({scheme_id:id})
 }
 
 function add(scheme){
