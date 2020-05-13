cabcae961c9215d4b4a0601aaa2982b8135de6be
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-13 16:28:07 -0600

    Quereis working

---
+++ schemes/scheme-model.js
@@ -0,0 +1,25 @@
+const db = require('../data/dbConfig');
+
+function find(){
+
+}
+
+function findById(id){
+    
+}
+
+function findSteps(id){
+    
+}
+
+function add(scheme){
+    
+}
+
+function update(changes, id){
+    
+}
+
+function remove(id){
+    
+}
